361258ba68a4132054f963d0ca5a985b1e2ec1b6
Alessandro Cagliostro Goncalves Neves <alecgn@hotmail.com>
2019-06-30 10:41:55 -0300

    commit 2019-06-30

---
+++ src/CryptHash.Net/lib/CryptHash.Net/Encryption/AES/AEAD/AEAD_AES_256_CBC_HMAC_SHA_256.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptHash.Net.Encryption.AES.AEAD
+{
+    public class AEAD_AES_256_CBC_HMAC_SHA_256
+    {
+
+    }
+}
