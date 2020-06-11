4ea5c2364137c2c7781113d71943ab8fd3e742a9
Alessandro Cagliostro Goncalves Neves <alecgn@hotmail.com>
2020-06-11 18:20:47 -0300

    initial drafts for RSA implementation #15

---
+++ src/CryptHash.Net/lib/CryptHash.Net/Encryption/RSA/EncryptionResults/RSADecryptionResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace CryptHash.Net.Encryption.RSA.EncryptionResults
+{
+    public class RSADecryptionResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public RSAParameters RSAParameters { get; set; }
+        public byte[] DecryptedDataBytes { get; set; }
+    }
+}
---
+++ src/CryptHash.Net/lib/CryptHash.Net/Encryption/RSA/EncryptionResults/RSAEncryptionResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace CryptHash.Net.Encryption.RSA.EncryptionResults
+{
+    public class RSAEncryptionResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public RSAParameters RSAParameters { get; set; }
+        public byte[] EncryptedDataBytes { get; set; }
+    }
+}
